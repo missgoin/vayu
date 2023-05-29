0a312839e7f41f23e6ab5b123aa1452a629b339a
missgoin <mister.goindo@gmail.com>
2023-05-29 16:41:58 +0700

    update sore

--- .circleci/vayu.sh
+++ .circleci/vayu.sh
@@ -1,136 +1,253 @@
 #!/usr/bin/env bash
+
+ #
  # Script For Building Android Kernel
-
-# Bail out if script fails
-set -e
-
-##--------------------------------------------------------##
-
-# Basic Information
-KERNEL_DIR="$(pwd)"
-VERSION=10
-MODEL=Xiaomi
-DEVICE=vayu
-DEFCONFIG=${DEVICE}_defconfig
-IMAGE=$(pwd)/out/arch/arm64/boot/Image
-DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
-DTB=$(pwd)/out/arch/arm64/boot/dts/qcom
-#C_BRANCH=$(git branch --show-current)
+ #
 
 ##----------------------------------------------------------##
-## Export Variables and Info
-function exports() {
-  export ARCH=arm64
-  export SUBARCH=arm64
-  #export LOCALVERSION="-${C_BRANCH}"
-  export KBUILD_BUILD_HOST=Pancali
-  export KBUILD_BUILD_USER="unknown"
-  export PROCS=$(nproc --all)
-  export DISTRO=$(source /etc/os-release && echo "${NAME}")
-  #export LC_ALL=C && export USE_CCACHE=1
-  #ccache -M 100G
+# Specify Kernel Directory
+KERNEL_DIR="$(pwd)"
 
-# Variables
+##----------------------------------------------------------##
+# Device Name and Model
+MODEL=POCO
+DEVICE=Vayu
+
+# Kernel Version Code
+VERSION=
+
+# Kernel Defconfig
+DEFCONFIG=vayu_defconfig
+
+# Select LTO variant ( Full LTO by default )
+DISABLE_LTO=0
+THIN_LTO=0
+
+# Files
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
+DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
+DTB=$(pwd)/out/arch/arm64/boot/dts/qcom
+
+# Verbose Build
+VERBOSE=0
+
+# Kernel Version
 KERVER=$(make kernelversion)
+
 COMMIT_HEAD=$(git log --oneline -1)
+
+# Date and Time
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 TANGGAL=$(date +"%F%S")
 
-# Compiler and Build Information
-TOOLCHAIN=trb # List (clang = zycrom | azure | aosp | nexus15 | proton )
-#LINKER=ld # List ( ld.lld | ld.bfd | ld.gold | ld )
-VERBOSE=0
-ClangMoreStrings=AR=llvm-ar NM=llvm-nm AS=llvm-as STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf
+# Specify Final Zip Name
+ZIPNAME=SUPER.KERNEL
+FINAL_ZIP=${ZIPNAME}-${DEVICE}-${TANGGAL}.zip
+FINAL_ZIP_ALIAS=Karenulvay-${TANGGAL}.zip
 
-FINAL_ZIP=SUPER.KERNEL-${TANGGAL}.zip
-FINAL_ZIP_ALIAS=Kernulvay-${TANGGAL}.zip
+##----------------------------------------------------------##
+# Specify compiler.
 
-}
+COMPILER=trb
+
+##----------------------------------------------------------##
+# Specify Linker
+LINKER=ld.lld
 
 ##----------------------------------------------------------##
 
-## Telegram Bot Integration
-
 ##----------------------------------------------------------##
+# Clone ToolChain
+function cloneTC() {
 
-## Get Dependencies
-function clone() {
-# Get Toolchain
-if [[ $TOOLCHAIN == "azure" ]]; then
-       git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang -b main clang
-elif [[ $TOOLCHAIN == "nexus14" ]]; then
-       git clone --depth=1 https://gitlab.com/Project-Nexus/nexus-clang.git -b nexus-14 clang
-elif [[ $TOOLCHAIN == "proton" ]]; then
-       git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
-elif [[ $TOOLCHAIN == "nexus15" ]]; then
-       git clone --depth=1 https://gitlab.com/Project-Nexus/nexus-clang.git -b nexus-15 clang
-elif [[ $TOOLCHAIN == "neutron" ]]; then
-       bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=latest
-       sudo apt install libelf-dev libarchive-tools
-       bash -c "$(wget -O - https://gist.githubusercontent.com/dakkshesh07/240736992abf0ea6f0ee1d8acb57a400/raw/e97b505653b123b586fc09fda90c4076c8030732/patch-for-old-glibc.sh)"
-elif [[ $TOOLCHAIN == "trb" ]]; then
-       git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git -b 17 clang
-fi
+	if [ $COMPILER = "atomx" ];
+	then
+	git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git clang
+	PATH="${KERNEL_DIR}/clang/bin:$PATH"
 
-# Get AnyKernel3
-git clone --depth=1 https://github.com/missgoin/AnyKernel3.git -b Vayu/Bhima AK3
+    elif [ $COMPILER = "neutron" ];
+    then
+    git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git clang
+    PATH="${KERNEL_DIR}/clang/bin:$PATH"
+    
+    elif [ $COMPILER = "trb" ];
+    then
+    git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git clang
+    PATH="${KERNEL_DIR}/clang/bin:$PATH"
 
-# Set PATH
-PATH="${KERNEL_DIR}/clang/bin:${PATH}"
+	elif [ $COMPILER = "azure" ];
+	then
+	git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang clang
+	PATH="${KERNEL_DIR}/clang/bin:$PATH"
 
-# Export KBUILD_COMPILER_STRING
-export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	elif [ $COMPILER = "proton" ];
+	then
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+	PATH="${KERNEL_DIR}/clang/bin:$PATH"
 
-}
+	elif [ $COMPILER = "eva" ];
+	then
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git -b gcc-new gcc64
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git -b gcc-new gcc32
+	PATH=$KERNEL_DIR/gcc64/bin/:$KERNEL_DIR/gcc32/bin/:/usr/bin:$PATH
 
+	elif [ $COMPILER = "aosp" ];
+	then
+        mkdir aosp-clang
+        cd aosp-clang || exit
+	wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r450784b.tar.gz
+        tar -xf clang*
+        cd .. || exit
+	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
+	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
+	PATH="${KERNEL_DIR}/aosp-clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+	fi
+	
+    # Clone AnyKernel
+    git clone --depth=1  https://github.com/Abhishek001konni/AnyKernel3.git
+
+	}
+	
+##------------------------------------------------------##
+# Export Variables
+function exports() {
+	
+        # Export KBUILD_COMPILER_STRING
+        if [ -d ${KERNEL_DIR}/clang ];
+           then
+               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+        elif [ -d ${KERNEL_DIR}/gcc64 ];
+           then
+               export KBUILD_COMPILER_STRING=$("$KERNEL_DIR/gcc64"/bin/aarch64-elf-gcc --version | head -n 1)
+        elif [ -d ${KERNEL_DIR}/aosp-clang ];
+            then
+               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/aosp-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+        fi
+        
+        # Export ARCH and SUBARCH
+        export ARCH=arm64
+        export SUBARCH=arm64
+        
+        # Export Local Version
+        #export LOCALVERSION="-${VERSION}"
+        
+        # KBUILD HOST and USER
+        export KBUILD_BUILD_HOST=Pancali
+        export KBUILD_BUILD_USER="unknown"
+        
+	    export PROCS=$(nproc --all)
+	    export DISTRO=$(source /etc/os-release && echo "${NAME}")
+	
+	}
+        
+##----------------------------------------------------------------##
+# Telegram Bot Integration
 ##----------------------------------------------------------------##
 
+# Export Configs
+function configs() {
+    if [ -d ${KERNEL_DIR}/clang ] || [ -d ${KERNEL_DIR}/aosp-clang  ]; then
+       if [ $DISABLE_LTO = "1" ]; then
+          sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/' arch/arm64/configs/cust_defconfig
+          sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/' arch/arm64/configs/cust_defconfig
+          sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/' arch/arm64/configs/cust_defconfig
+       elif [ $THIN_LTO = "1" ]; then
+          sed -i 's/# CONFIG_THINLTO is not set/CONFIG_THINLTO=y/' arch/arm64/configs/cust_defconfig
+       fi
+    elif [ -d ${KERNEL_DIR}/gcc64 ]; then
+       sed -i 's/CONFIG_LLVM_POLLY=y/# CONFIG_LLVM_POLLY is not set/' arch/arm64/configs/cust_defconfig
+       sed -i 's/# CONFIG_GCC_GRAPHITE is not set/CONFIG_GCC_GRAPHITE=y/' arch/arm64/configs/cust_defconfig
+       if ! [ $DISABLE_LTO = "1" ]; then
+          sed -i 's/# CONFIG_LTO_GCC is not set/CONFIG_LTO_GCC=y/' arch/arm64/configs/cust_defconfig
+       fi
+    fi
+}
+
+##----------------------------------------------------------##
+# Compilation
 function compile() {
 START=$(date +"%s")
-
-# Generate .config
-make O=out ARCH=arm64 ${DEFCONFIG} LLVM=1 LLVM_IAS=1
-
-# Start Compilation
-if [[ "$TOOLCHAIN" == "azure" ]]; then
-     make -j$(nproc --all) O=out ARCH=$ARCH CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CONFIG_NO_ERROR_ON_MISMATCH=y V=$VERBOSE 2>&1 | tee error.log
-elif [[ "$TOOLCHAIN" == "proton" ]]; then
-     make -j$(nproc --all) O=out ARCH=$ARCH CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip V=$VERBOSE 2>&1 | tee error.log
-elif [[ "$TOOLCHAIN" == "nexus" ]]; then
-     make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 V=$VERBOSE 2>&1 | tee error.log
-elif [[ "$TOOLCHAIN" == "neutron" ]]; then 
-     make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CONFIG_NO_ERROR_ON_MISMATCH=y V=$VERBOSE 2>&1 | tee error.log
-elif [[ "$TOOLCHAIN" == "trb" ]]; then
-     make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 $ClangMoreStrings V=$VERBOSE 2>&1 | tee error.log
-fi
-	
+		
+	# Compile
+	make O=out ${DEFCONFIG}
+	if [ -d ${KERNEL_DIR}/clang ];
+	   then
+	       make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CC=clang \
+	       HOSTCC=clang \
+	       HOSTCXX=clang++ \
+	       CROSS_COMPILE=aarch64-linux-gnu- \
+	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	       LD=${LINKER} \
+	       AR=llvm-ar \
+	       NM=llvm-nm \
+	       OBJCOPY=llvm-objcopy \
+	       OBJDUMP=llvm-objdump \
+	       STRIP=llvm-strip \
+	       READELF=llvm-readelf \
+	       OBJSIZE=llvm-size \
+	       V=$VERBOSE 2>&1 | tee error.log
+	elif [ -d ${KERNEL_DIR}/gcc64 ];
+	   then
+	       make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CROSS_COMPILE_ARM32=arm-eabi- \
+	       CROSS_COMPILE=aarch64-elf- \
+	       LD=aarch64-elf-${LINKER} \
+	       AR=llvm-ar \
+	       NM=llvm-nm \
+	       OBJCOPY=llvm-objcopy \
+	       OBJDUMP=llvm-objdump \
+	       STRIP=llvm-strip \
+	       OBJSIZE=llvm-size \
+	       V=$VERBOSE 2>&1 | tee error.log
+    elif [ -d ${KERNEL_DIR}/aosp-clang ];
+       then
+           make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CC=clang \
+           HOSTCC=clang \
+	       HOSTCXX=clang++ \
+	       CLANG_TRIPLE=aarch64-linux-gnu- \
+	       CROSS_COMPILE=aarch64-linux-android- \
+	       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+	       LD=${LINKER} \
+	       AR=llvm-ar \
+	       NM=llvm-nm \
+	       OBJCOPY=llvm-objcopy \
+	       OBJDUMP=llvm-objdump \
+           STRIP=llvm-strip \
+	       READELF=llvm-readelf \
+	       OBJSIZE=llvm-size \
+	       V=$VERBOSE 2>&1 | tee error.log
+	fi
 }
 
 ##----------------------------------------------------------------##
-
 function zipping() {
-# Copy Files To AnyKernel3 Zip
-cp $IMAGE AK3
-cp $DTBO AK3
-cp $DTBO AK3/dir
-
-# Zipping and Upload Kernel
-cd AK3 || exit 1
-zip -r9 ${FINAL_ZIP_ALIAS} *
-MD5CHECK=$(md5sum "$FINAL_ZIP_ALIAS" | cut -d' ' -f1)
-echo "Zip: $FINAL_ZIP_ALIAS"
-curl -T $FINAL_ZIP_ALIAS https://oshi.at; echo
-
-cd ..
-
+	# Copy Files To AnyKernel3 Zip
+	cp $IMAGE AnyKernel3
+	cp $DTBO AnyKernel3
+	find $DTB -name "*.dtb" -exec cat {} + > AnyKernel3/dtb
+	
+	# Zipping and Push Kernel
+	cd AnyKernel3 || exit 1
+        zip -r9 ${FINAL_ZIP_ALIAS} *
+        MD5CHECK=$(md5sum "$FINAL_ZIP_ALIAS" | cut -d' ' -f1)
+        echo "Zip: $FINAL_ZIP_ALIAS"
+        curl -T $FINAL_ZIP_ALIAS https://oshi.at; echo
+    cd ..
 }
-
+    
 ##----------------------------------------------------------##
 
-# Functions
+cloneTC
 exports
-clone
+configs
 compile
+END=$(date +"%s")
+DIFF=$(($END - $START))
 zipping
 
-##------------------------*****-----------------------------##
+##----------------*****-----------------------------##
